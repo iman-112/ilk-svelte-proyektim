0b525c9342e87c2cf059328a7cc2f190cc2ffbd3
imanxelilov <iman.xeliov@mail.ru>
2022-05-06 10:57:27 -0700

    first commit

---
+++ src/routes/contact.svelte
@@ -0,0 +1,66 @@
+
+
+<script>
+
+</script>
+<main>
+    <div class="container">
+<div class="nav">
+    <div class="solteref">
+        <h1>Iman</h1>
+    </div>
+    <div class="sag-teref">
+        <div class="link">
+            <a href="/haqqimda">Haqqimda</a>
+            <a href="/CV">CV`m</a>
+            <a href="/contact">Elaqe</a>
+        </div>
+    </div>
+</div>
+
+
+</div>
+</main>
+
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    .container{
+        background-color:#f0e4e4 ;
+    }
+    .container{
+        width: 100%;
+        height: 100vh;
+    }
+.nav{
+    display: flex;
+    height:47px;
+    width: 100%;
+    justify-content: space-around;
+    background-color: #2C3E50;
+}
+.solteref h1{
+    padding: .5rem;
+    color: #fff;
+    cursor: pointer;
+    font-size: 2rem;
+ 
+}
+ .nav .sag-teref{
+        padding: 1rem;
+ }
+  .nav .sag-teref a{
+        color: #fff;
+        padding-right: 3rem;
+        font-size:20px;
+        text-decoration: none;
+        transition: all .3s;
+        
+    }
+    .nav .sag-teref a:hover{
+        color: rgb(20, 18, 18);
+    }
+  
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,94 @@
+
+
+<script>
+
+</script>
+<main>
+    <div class="container">
+<div class="nav">
+    <div class="solteref">
+        <a href="">Iman</a> 
+    </div>
+    <div class="sag-teref">
+        <div class="link">
+            <a href="/haqqimda">Haqqimda</a>
+            <a href="/CV">CV`m</a>
+            <a href="/contact">Elaqe</a>
+        </div>
+    </div>
+</div>
+
+<section>
+    <div class="img-content">
+        
+        <img src="src/img/profile.png" alt="">
+        <h1>Iman Xelilov Web Sayti</h1>
+
+
+    </div>
+
+
+</section>
+
+</div>
+</main>
+
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    .container{
+        background-color:#f0e4e4 ;
+    }
+    .container{
+        width: 100%;
+        height: 100vh;
+    }
+.nav{
+    display: flex;
+    height:47px;
+    width: 100%;
+    justify-content: space-around;
+    background-color: #2C3E50;
+}
+.solteref a{
+    padding: .5rem;
+    color: #fff;
+    cursor: pointer;
+    font-size: 2rem;
+    text-decoration: none;
+}
+ .nav .sag-teref{
+        padding: 1rem;
+ }
+  .nav .sag-teref a{
+        color: #fff;
+        padding-right: 3rem;
+        font-size:20px;
+        text-decoration: none;
+        transition: all .3s;
+        
+    }
+    .nav .sag-teref a:hover{
+        color: rgb(20, 18, 18);
+    }
+    .img-content{
+        text-align: center;
+        justify-content: center;
+
+    }
+    .img-content h1{
+    
+        margin-top: 4rem;
+        color: black;
+        text-transform:uppercase;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
+    .img-content img{
+        width: 300px;
+        height: auto;
+        transform: translateX(-80%);
+        transform: translateY(20%);
+}
+</style>
